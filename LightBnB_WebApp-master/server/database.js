e8764f4bec8fd50fb257c683b8b71d30160b09d1
Vitaly Zaff <vitalyzavgorodnev@gmail.com>
2021-05-27 15:49:30 +0000

    LighthouseBnB Reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -72,7 +72,12 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = 'SELECT * FROM reservations JOIN properties ON property_id = properties.id JOIN property_reviews ON reservations.id = reservation_id WHERE guest_id = $1 AND end_date != NOW() LIMIT $2;';
+  const values = [guest_id, limit];
+  return pool
+    .query(query, values)
+    .then((result) => result.rows)
+    .catch((err) => err.message);
 };
 exports.getAllReservations = getAllReservations;
 
